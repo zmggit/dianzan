64c4f60a2df6a3e0a6d3ea7c28368385faa67bf6
zmggit <1551605159@qq.com>
2026-06-17 16:30:21 +0800

    点赞工具

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App'
+import './uni.promisify.adaptor'
+
+import Api from './request/api'
+Vue.prototype.$api = Api
+
+import utils from './utils/index.js'
+Vue.prototype.$util = utils
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+  ...App
+})
+app.$mount()
